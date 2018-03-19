b3c6833cce32107ebaebb873497102c40324b23a
adam n <adam.nawroc@gmail.com>
2018-03-19 11:04:48 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(name, grade, id=nil)
+  def initialize(id=nil, name, grade)
     @name = name
     @grade = grade
     @id = id
@@ -50,5 +50,36 @@ class Student
  end
 
 end
+
+def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    p row
+  # create a new Student object given a row from the database
+  new_student = self.new(row[0], row[1], row[2])  # self.new is the same as running Song.new
+  # new_student.id = row[0]
+  # new_student.name =  row[1]
+  # new_student.grade = row[2]
+  new_student  # return the newly created instance
+end
+
+def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 end
